d4c481450e9c38863e38cf153dbc1898bd27f6fa
Your Name <you@example.com>
2017-10-09 02:35:07 -0700

    new commit

--- Docker-CI-tools/Sonar/generic/dependency-check/run.sh
+++ Docker-CI-tools/Sonar/generic/dependency-check/run.sh
@@ -10,17 +10,38 @@ while [ $# -gt 0 ]; do
   shift
 done
 
-echo $sourcerepo
-echo $folder
+if [ -d $projectFolder ]; then rm -rf $projectFolder; fi
+git clone $sourceRepo
 
-#if [ -d $folder ]; then rm -rf $folder; fi
-#git clone $sourcerepo
+ls
+
+./../dependency-check/bin/dependency-check.sh --project $projectFolder  --format "XML" --out . --scan $sourceToScan/**
+
+if [ -d $resultFolder ]; then rm -rf $resultFolder; fi
+git clone $resultRepo
 
 
-#./../dependency-check/bin/dependency-check.sh --project $folder  --format "XML" --out . $scan $source/**
+cp dependency-check-report.xml $resultFolder
 
-#git clone $resultrepo
+cd $resultFolder
 
-#git add .
-#git commit -m "From dependency-check"
-#git push  https://$username:$password@github.com/RiiecCo/results.git --all
+#git config --global user.email "you@example.com"
+#git config --global user.name "Your Name"
+git init
+git add -A
+git commit -m "From dependency-check"
+git push https://$username:$password@github.com/RiiecCo/results.git 
+
+: <<'END'
+Tool usage example:
+
+sudo docker run dep  \
+--sourceRepo https://github.com/RiieCco/dockertools.git \
+--projectFolder dockertools/testable-code \
+--sourceToScan dockertools/testable-code/pythonflask \
+--resultRepo http://github.com/riiecco/results.git \
+--resultFolder results \
+--username Riiecco \
+--password Lakers112
+
+END
