a729a010cd893636d6e23053e90e0220fdcee359
Riccardo ten Cate <r.tencate77@gmail.com>
2017-11-01 02:55:21 -0700

    Update run.sh

--- security/zapAPI/run.sh
+++ security/zapAPI/run.sh
@@ -10,22 +10,19 @@ while [ $# -gt 0 ]; do
   shift
 done
 
-if [ -d $projectFolder ]; then rm -rf $projectFolder; fi
-git clone $sourceRepo
 
-cd $sourceToScan
-
-retire
+python api-scan-auth-header.py  --zap_key $zap_key --zap_proxy $zap_proxy --target $target --swagger $swagger --auth_token $auth_token 
 
 curl --insecure -H 'Accept: application/json' -X POST --form "file=@./dependency-check-report.xml"
 
 : <<'END'
 Tool usage example:
 
-sudo docker run retire  \
---sourceRepo https://github.com/riiecco/dockertools.git \
---projectFolder testable-code \
---sourceToScan testable-code/pythonflask \
---resultSource https://172.17.0.1:8443/threadfix/rest/applications/1/upload?apiKey={7M8Uw9RLqkobJJe1rcIHElOSGbTuAAuUHHNpgmMVP58}
-
+sudo docker run zapAPI  \
+--zap_key ASD235457rgfSfds
+--zap_proxy http://127.0.0.1:8080
+--target https://example.com
+--swagger https://example.com/api/swagger.api
+--auth_token eydfsgDFGHdgbdfjfhghj.dsgjkdg@234tdw57dsg.3590dsfl*45532dfs
+--dasboard https://threadfix.com/foobarsdasdas
 END
