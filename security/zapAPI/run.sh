50cb91e58144ad367b16dfd36f51992229214305
Riccardo ten Cate <r.tencate77@gmail.com>
2017-11-10 12:29:26 +0100

    Update run.sh

--- security/zapAPI/run.sh
+++ security/zapAPI/run.sh
@@ -14,7 +14,8 @@ if [ -d $projectFolder ]; then rm -rf $projectFolder; fi
 git clone $sourceRepo
 
 cd $projectFolder
-./run-with-docker run it:test-only *UserApiRoutesIntegrationSpec
+trap 'sudo docker-compose stop' SIGINT
+sudo docker-compose run --rm --publish 9000:9000 app sbt it:test-only *UserApiRoutesIntegrationSpec
 
 python api-scan-auth-header.py --zap_key $zap_key --zap_proxy $zap_proxy --target $target --swagger $swagger --auth_token $auth_token
 
