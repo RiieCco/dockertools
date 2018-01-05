9ddcaf485ca8414fb3a4019a4fad55197a17432f
Riccardo ten Cate <r.tencate77@gmail.com>
2018-01-05 02:35:28 -0800

    Update entrypoint.sh

--- security/retireJS/entrypoint.sh
+++ security/retireJS/entrypoint.sh
@@ -39,7 +39,7 @@ main() {
     npm install
 
     #nsp check --reporter json
-    retire --outputformat json
+    retire -p
     echo "output"
     retire --outputpath "${RESULT_FILE}"
     echo "path"
