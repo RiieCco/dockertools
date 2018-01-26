d1960d6ffcffa51fc3735d14d2bbfafbbcc75e77
Riccardo ten Cate <r.tencate77@gmail.com>
2018-01-26 04:10:49 -0800

    Update entrypoint.sh

--- security/anchore/entrypoint.sh
+++ security/anchore/entrypoint.sh
@@ -12,23 +12,14 @@ ANCHORE_BIN=/usr/local/bin/anchore-cli
 [ -z "${ANCHOR_USER}" ] && exit_env_error ANCHOR_USER
 [ -z "${ANCHOR_PASS}" ] && exit_env_error ANCHOR_PASS
 [ -z "${ANCHOR_URL}" ] && exit_env_error ANCHOR_URL
+[ -z "${IMAGES}" ] && exit_env_error IMAGES
 
 #authentication to the registry
-#anchore-cli --u $anchor_user --p $anchor_pass --url $anchor_url registry add $registry_url $registry_user $registry_pass
-#anchore-cli --u $anchor_user --p $anchor_pass --url $anchor_url registry get docker.tntdigital.io
+#anchore-cli --u $ANCHOR_USER --p $ANCHOR_PASS --url $ANCHOR_URL registry add $REGISTRY_URL $REGISTRY_USER $REGISTRY_PASS
+#anchore-cli --u $ANCHOR_USER --p $ANCHOR_PASS --url $ANCHOR_URL registry get $REGISTRY_URL
 
 #analyzing and scanning the target
 "${ANCHORE_BIN}" --u "${ANCHOR_USER}" --p "${ANCHOR_PASS}" --url "${ANCHOR_URL}" image list
-"${ANCHORE_BIN}" --u "${ANCHOR_USER}" --p "${ANCHOR_PASS}" --url "${ANCHOR_URL}" image add docker.io/library/debian:latest
-"${ANCHORE_BIN}" --u "${ANCHOR_USER}" --p "${ANCHOR_PASS}" --url "${ANCHOR_URL}" --json image vuln docker.io/library/debian:latest os
+"${ANCHORE_BIN}" --u "${ANCHOR_USER}" --p "${ANCHOR_PASS}" --url "${ANCHOR_URL}" image add "${IMAGES}"
+"${ANCHORE_BIN}" --u "${ANCHOR_USER}" --p "${ANCHOR_PASS}" --url "${ANCHOR_URL}" --json image vuln "${IMAGES}" os
 
-# Tool usage example:
-# docker run --rm -it local/anchore-cli:latest
-# --anchor_user admin
-# --anchor_pass foobar
-# --anchor_url http://localhost/:8228/v1
-# --registry_user test
-# --registry_pass test
-# --registry_url https://docker.company.io
-# --images  docker.company.io/image:foo, docker.company.io/image:bar, etc
-# --resultSource https://172.17.0.1:8443/threadfix/rest/applications/1/upload?apiKey={AgdLRap39VUEnBUajxZjoeKKunIv7psnBeH33vFfU}
