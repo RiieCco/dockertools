cede3f2becbba953ff4b8ddeddca813c0d972483
Riccardo ten Cate <r.tencate77@gmail.com>
2018-01-26 05:01:21 -0800

    Update entrypoint.sh

--- security/anchore/entrypoint.sh
+++ security/anchore/entrypoint.sh
@@ -37,6 +37,14 @@ while IFS=',' read -ra IMAGE; do
           echo "$i is scanning now"
           "${ANCHORE_BIN}" --u "${ANCHOR_USER}" --p "${ANCHOR_PASS}" --url "${ANCHOR_URL}" --json image vuln "$i" os
           echo "scan for $i is done"
+          
+          #Here we will also post results to the JIRA API
+          #curl -H "Content-Type: application/json" -X POST -d "$(sprintf '{"username": "%s", "password":"%s"}' "${JIRA_USER}" "${JIRA_PASSWD}")" "${JIRA_PROXY_BASEURL}/Login?token=${JIRA_API_TOKEN}"
+          #curl -H "Content-Type: application/json" -X GET "${JIRA_PROXY_BASEURL}/SyncIssue/Update?token=${JIRA_API_TOKEN}"
+          #curl -H "Content-Type: application/json" -X POST --form "files=@${RESULT_FILE}" "${JIRA_PROXY_BASEURL}/RetireJS?token=${JIRA_API_TOKEN}"
+          #curl -H "Content-Type: multipart/form-data" -X POST -F "files=@${RESULT_FILE};filename=retire.json" "${JIRA_PROXY_BASEURL}/RetireJS?token=${JIRA_API_TOKEN}"
+          #curl -H "Content-Type: application/json" -X GET  "${JIRA_PROXY_BASEURL}/SyncIssue/New?token=${JIRA_API_TOKEN}"
+         
       done
  done <<< "${IMAGES}"
 
