9ee42289db40f9687256de9fc4290d05efe393a5
Riccardo ten Cate <r.tencate77@gmail.com>
2018-02-06 03:18:31 -0800

    Update entrypoint.sh

--- security/zapAPI/entrypoint.sh
+++ security/zapAPI/entrypoint.sh
@@ -21,7 +21,7 @@ RESULT_FILE="/tmp/$$.$(hostname).results.json"
 #[ -z "${JIRA_PASSWD}" ]  && exit_env_error JIRA_PASSWD
 
 
-token=$(curl -H "Content-Type: application/json" -X POST -d  '{"email": "riccardotencate@gmail.com", "password":"Welkom01!"}' "http://localhost:3000/rest/user/login" | cut -d ':' -f 3 | cut -d '"' -f 2)
+token=$(curl -H "Content-Type: application/json" -X POST -d  '{"email": "riccardotencate@gmail.com", "password":"Welkom01!"}' "http://172.17.0.1:3000/rest/user/login" | cut -d ':' -f 3 | cut -d '"' -f 2)
 
 
 python /api-scan-auth-header.py --zap_key "${ZAP_KEY}" --zap_proxy "${ZAP_PROXY}" --target "${TARGET}" --swagger "${SWAGGER}" --auth_token "${token}"
