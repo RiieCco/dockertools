0cc725512427caa002694291af88768823762dad
Riccardo ten Cate <r.tencate77@gmail.com>
2018-04-23 21:43:02 +0200

    Update test.sh

--- security/test.sh
+++ security/test.sh
@@ -1,6 +1,6 @@
 curl https://raw.githubusercontent.com/RiieCco/dockertools/master/security/diva.apk > diva.apk
 echo "curl"
-export HASH="$(curl -F 'file=@/Users/riiecco/Desktop/diva.apk' http://192.168.2.23:8000/api/v1/upload -H "Authorization:39f6fd6d1c6ef13a1b81445ebcdc6f135c5d15ac7b6c0c8507a19f89a1988765" | grep "hash" | cut -d: -f2 | cut -d \" -f2)"
+export HASH="$(curl -F 'file=@${WORKSPACE}/diva.apk' http://192.168.2.23:8000/api/v1/upload -H "Authorization:39f6fd6d1c6ef13a1b81445ebcdc6f135c5d15ac7b6c0c8507a19f89a1988765" | grep "hash" | cut -d: -f2 | cut -d \" -f2)"
 echo "export"
 echo $HASH
 curl -X POST --url http://192.168.2.23:8000/api/v1/scan --data "scan_type=apk&file_name=diva.apk&hash=${HASH}" -H "Authorization:39f6fd6d1c6ef13a1b81445ebcdc6f135c5d15ac7b6c0c8507a19f89a1988765"
