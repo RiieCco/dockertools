93134fa20fd1149b0173a93bcf8db9bf1b19c870
Riccardo ten Cate <riiecco@Riccardos-MacBook-Pro.local>
2018-05-21 22:05:49 +0200

    updates

--- security/retireJS/entrypoint.sh
+++ security/retireJS/entrypoint.sh
@@ -11,17 +11,15 @@ DEFAULT_RESULT_FILE="$(hostname).results.json"
 RESULT_FILE="${RESULT_FILE:-${DEFAULT_RESULT_FILE}}"
 FOLDER="${FOLDER:-/application}"
 
-[ -z "${REPO}" ] && exit_env_error REPO
+[ -z "${IS_NPM}" ] && exit_env_error APPLICATION_ROOT
+[ -z "${DOJO_URL}" ] && exit_env_error DOJO_URL
+[ -z "${DOJO_ENGAGEMENT_ID}" ] && exit_env_error DOJO_ENGAGEMENT_ID
+[ -z "${DOJO_API_KEY}" ] && exit_env_error DOJO_API_KEY
 
 
 #[ -z "${ARTIFACTORY_USER}" ] && exit_env_error ARTIFACTORY_USER
 #[ -z "${ARTIFACTORY_EMAIL}" ] && exit_env_error ARTIFACTORY_EMAIL
 #[ -z "${ARTIFACTORY_PWHASH}" ] && exit_env_error ARTIFACTORY_PWHASH
-
-#[ -z "${JIRA_PROXY_BASEURL}" ] && exit_env_error JIRA_PROXY_BASEURL # http://172.17.0.1:1337/api
-#[ -z "${JIRA_API_TOKEN}" ]  && exit_env_error JIRA_API_TOKEN
-#[ -z "${JIRA_USER}" ]  && exit_env_error JIRA_USER
-#[ -z "${JIRA_PASSWD}" ]  && exit_env_error JIRA_PASSWD
 
 #export ARTIFACTORY_USER=${ARTIFACTORY_USER}
 #export ARTIFACTORY_EMAIL=${ARTIFACTORY_EMAIL}
@@ -29,15 +27,22 @@ FOLDER="${FOLDER:-/application}"
 
 #npm config set @artifactory:registry https://company.artifactoryonline.com/company/api/npm/npm-local/
 
-if [ -d "${FOLDER}" ]; then rm -rf "${FOLDER}"; fi
-git clone "${REPO}" "${FOLDER}"
 
-cd "${FOLDER}"
-npm install
+cd "${FOLDER}/${APPLICATION_ROOT}"
+if [ "$IS_NPM" == "True" ]; then
+    npm install
+    retire -p --outputformat json 
+    retire --outputpath "${RESULT_FILE}"
+    curl --request POST --url "${DOJO_URL}"/api/v1/importscan/ --header 'authorization: ApiKey '"${DOJO_API_KEY}"' ' --header 'cache-control: no-cache' --header 'content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' --form minimum_severity=Info --form scan_date=2018-05-01 --form verified=False --form file=@"${RESULT_FILE}" --form tags=test_automation --form scan_type='Dependency Check Scan'
+else
+    retire -p --outputformat json 
+    retire --outputpath "${RESULT_FILE}"
+    curl --request POST --url "${DOJO_URL}"/api/v1/importscan/ --header 'authorization: ApiKey '"${DOJO_API_KEY}"' ' --header 'cache-control: no-cache' --header 'content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' --form minimum_severity=Info --form scan_date=2018-05-01 --form verified=False --form file=@"${RESULT_FILE}" --form tags=test_automation --form scan_type='Dependency Check Scan'
 
-retire -p --outputformat json 
-retire --outputpath "${RESULT_FILE}"
-     
+fi
+
+
+
 #curl -H "Content-Type: application/json" -X POST -d "$(sprintf '{"username": "%s", "password":"%s"}' "${JIRA_USER}" "${JIRA_PASSWD}")" "${JIRA_PROXY_BASEURL}/Login?token=${JIRA_API_TOKEN}"
 #curl -H "Content-Type: application/json" -X GET "${JIRA_PROXY_BASEURL}/SyncIssue/Update?token=${JIRA_API_TOKEN}"
 #curl -H "Content-Type: application/json" -X POST --form "files=@${RESULT_FILE}" "${JIRA_PROXY_BASEURL}/RetireJS?token=${JIRA_API_TOKEN}"
